a14bf0bd8dd3a12ab48070c482edf6c5a2d53dc8
Sana <Sana-Rasheed@users.noreply.github.com>
2023-05-06 12:40:02 -0400

    Delete Solidity codes directory

--- Solidity codes/20.15-Mathematical function/3-Cryptographic functions.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract Test {   
-   function callKeccak256() public pure returns(bytes32 result){
-      return keccak256("ABC");
-   }  
-}
--- Solidity codes/20.3-Comments and variables/1-State variable.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.0;
-contract SolidityTest {
-   uint storedData;      // State variable
-   constructor() public {
-      storedData = 10;   // Using State variable
-   }
-}
